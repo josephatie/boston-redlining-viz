969b87405466f0b8e9607143def9f549a59c25d0
josephatie <joseph.atie@gmail.com>
2025-04-29 10:22:27 -0400

    postcss changes

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,8 +1,8 @@
 // postcss.config.cjs
 module.exports = {
   plugins: {
-    // use the new package name here:
-    '@tailwindcss/postcss': {},
-    'autoprefixer': {},
+    // ← use the canonical plugin name Tailwind expects
+    tailwindcss: {},
+    autoprefixer: {},
   },
 };
